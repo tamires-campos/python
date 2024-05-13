4a9842193ec02f7514a1d1a65219dc9c64d7a6d1
Tamires Campos Pletti <tamiresalvacampos9@gmail.com>
2024-05-13 17:18:54 -0300

    primeiro exercicio

---
+++ exercícios/ex001.py
@@ -0,0 +1,24 @@
+print('Olá, mundo!')
+# tudo o que forma digitar que são frases ou palavras, temos que usar aspas "blablabla"
+
+print(7 + 4)
+# mas se forma digitar números, não precisamos usar aspas e nesta exeplo ele me daria o resultado = 11
+
+print("7"+"4")
+# aqui ele juntaria as informações e daria resultado = 74 #
+
+nome = "Tamires"
+idade = 24
+peso = 67
+#aqui estou representando variáveis, que logo em seguida resebe um sinal de "=" que em python sig. "recebe"
+print(nome, idade, peso)
+
+
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+peso = input("Quanto você pesa? ")
+print(nome, idade, peso)
+
+
+
+
