3aa2185524f3fe0db15b3f84cf4d6ac882f2b8fd
Tamires Campos Pletti <tamiresalvacampos9@gmail.com>
2024-05-17 16:57:16 -0300

    consegui colocar audio

--- desafios/des021.py
+++ desafios/des021.py
@@ -1,1 +1,8 @@
-ddq
+"""from playsound import playsound
+playsound('ex021.mp3')
+"""
+"""from playsound import playsound
+playsound('arquivo.mp3')"""
+
+from playsound3 import playsound
+playsound('C:/Users/tamir_mwwail1/Documents/Estudos/python/desafios/audio/audio.mp3')
