c608c8cff692451c115b8df953c857e445e46538
Tamires Campos Pletti <tamiresalvacampos9@gmail.com>
2024-06-18 11:32:52 -0300

    novos desafios

--- desafios/des062.py
+++ desafios/des062.py
@@ -6,10 +6,14 @@ primeiro = int(input("Primeiro termo: "))
 razão = int(input("Razão da PA: "))
 termo = primeiro
 cont = 1
-
-while cont <= 10:
-    print(termo, "-> ", end="")
-    termo = termo + razão
-    cont += 1
-    
-print("Fim")
+total = 0
+mais = 10    # mais recebe 10 termos, pois ele obrigatóriamente irá mostrar 10 termos e depois irá somar caso queira  
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(termo, "-> ", end="")
+        termo = termo + razão
+        cont += 1
+    print("Pausa")
+    mais = int(input("Quantos termos você quer mostrar a mais? ")) 
+print(f"Progressão finalizada com {total} termos mostrados")
---
+++ desafios/des063.py
@@ -0,0 +1,4 @@
+print("-" * 20)
+print("Sequência de Fibinacci")
+print("-" * 20)
+print("Quantos termos você quer mostrar? ")
